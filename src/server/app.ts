14ab347d81b615a6388c72e28d310fba3509a11e
vivek upadhyay <viveku407@gmail.com>
2024-11-23 09:59:56 +0530

    commit: resolve cors issues

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,16 +1,19 @@
-import express from 'express'
-import {db} from '../config/db.config'
-import { router } from '../routes/sportsClub.routes'
-import {playersRoute} from '../routes/players.routes'
-const port = process.env.PORT
-const app=express()
+import express from "express";
+import { db } from "../config/db.config";
+import { router } from "../routes/sportsClub.routes";
+import { playersRoute } from "../routes/players.routes";
+import cors from "cors";
 
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+const port = process.env.PORT;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 //routes
-app.use('/api/v1/clubs', router)
-app.use('/api/v1/players', playersRoute)
+app.use("/api/v1/clubs", router);
+app.use("/api/v1/players", playersRoute);
 
 db.then(() => {
-    app.listen(7070, () => console.log('Server is listening on port 7070'))
-})
+  app.listen(7070, () => console.log("Server is listening on port 7070"));
+});
